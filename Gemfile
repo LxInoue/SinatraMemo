80a24a0d112d225c9bd3b0b30a022ad8f19359b9
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-13 11:32:04 +0900

    メモ一覧の画面を追加

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'json'
 
 gem "rackup", "~> 2.2"
 gem "puma", "~> 6.6"
--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
-get '/' do
-  
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+before do
+  @app_name = 'メモアプリ'
+end
+
+def load_memos
+  return [] unless File.exist?('memos.json')
+  JSON.parse(File.read('memos.json'), symbolize_names: true)
+end
+
+get '/memos' do
+  @memos = load_memos
+  erb :index
 end
